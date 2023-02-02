8d4c2b996ae66c448de2de1d545bd32568281542
kskim <kskim@seojinsys.co.kr>
2023-02-02 15:25:24 +0900

    불필요 삭제

--- src/main/java/com/seojin/batch/sys/constant/Const.java
+++ src/main/java/com/seojin/batch/sys/constant/Const.java
@@ -13,11 +13,7 @@ import com.seojin.commons.constant.BaseConstants;
  * */
 public class Const extends BaseConstants {
 
-	/**
-	 * 배치 시스템 ID
-	 */
-	public static final String BATCH_SYSTEM_ID = "BATCH-SVR1";
-
+	
 	/**
 	 * SCHEDULE_PARAMS
 	 */
